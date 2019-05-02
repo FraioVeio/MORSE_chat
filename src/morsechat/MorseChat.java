13a7c018ffba776eda045f9fc2c72556dcaf8273
FraioVeio <fraiomobile@gmail.com>
2019-05-02 16:02:33 +0200

    add send function

--- src/morsechat/MorseChat.java
+++ src/morsechat/MorseChat.java
@@ -16,11 +16,11 @@ public class MorseChat extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         text1 = new javax.swing.JTextArea();
         sendButton = new javax.swing.JButton();
-        receiveButton = new javax.swing.JButton();
         statusField = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         speedField = new javax.swing.JTextField();
         pb = new javax.swing.JProgressBar();
+        receiveButton = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("MORSE chat");
@@ -39,19 +39,19 @@ public class MorseChat extends javax.swing.JFrame {
             }
         });
 
+        statusField.setEditable(false);
+
+        jLabel1.setText("Velocità (ms):");
+
+        speedField.setHorizontalAlignment(javax.swing.JTextField.TRAILING);
+        speedField.setText("60");
+
         receiveButton.setText("Ricevi");
         receiveButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 receiveButtonActionPerformed(evt);
             }
         });
-
-        statusField.setEditable(false);
-
-        jLabel1.setText("Velocità (ms):");
-
-        speedField.setHorizontalAlignment(javax.swing.JTextField.TRAILING);
-        speedField.setText("10");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -82,9 +82,9 @@ public class MorseChat extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(sendButton)
-                    .addComponent(receiveButton)
                     .addComponent(jLabel1)
-                    .addComponent(speedField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(speedField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(receiveButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(statusField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -95,11 +95,23 @@ public class MorseChat extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void sendButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendButtonActionPerformed
+        sendButton.setEnabled(false);
+        receiveButton.setEnabled(false);
+        pb.setIndeterminate(true);
+        statusField.setText("");
         
+        MorseThread mt = new MorseThread(this, MorseTranslator.asciiToMorse(text1.getText()), Float.parseFloat(speedField.getText()));
+        mt.start();
     }//GEN-LAST:event_sendButtonActionPerformed
 
     private void receiveButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_receiveButtonActionPerformed
-        
+        if(receiveButton.isSelected()) {
+            // Ascolta
+            
+        } else {
+            // Smetti di ascoltare
+            
+        }
     }//GEN-LAST:event_receiveButtonActionPerformed
 
     public static void main(String args[]) {
@@ -115,11 +127,11 @@ public class MorseChat extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JProgressBar pb;
-    private javax.swing.JButton receiveButton;
-    private javax.swing.JButton sendButton;
+    public javax.swing.JProgressBar pb;
+    public javax.swing.JToggleButton receiveButton;
+    public javax.swing.JButton sendButton;
     private javax.swing.JTextField speedField;
-    private javax.swing.JTextField statusField;
+    public javax.swing.JTextField statusField;
     private javax.swing.JTextArea text1;
     // End of variables declaration//GEN-END:variables
 }
--- src/morsechat/MorseThread.java
+++ src/morsechat/MorseThread.java
@@ -1,19 +1,123 @@
 package morsechat;
 
+import java.util.ArrayList;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.SourceDataLine;
+
 public class MorseThread extends Thread {
+    private MorseChat frame;
     private boolean send;
     private String msg;
+    private float duration;
     
-    public MorseThread(boolean send) {
+    
+    private byte[] buf;
+    private AudioFormat af;
+    private SourceDataLine sdl;
+    
+    public volatile boolean done;
+    
+    private MorseThread(MorseChat frame, boolean send) {
+        this.frame = frame;
         this.send = send;
+        done = false;
+    }
+    
+    public MorseThread(MorseChat frame, String msg, float duration) {
+        this(frame, true);
+        
+        this.msg = msg;
+        this.duration = duration;
     }
     
     @Override
     public void run() {
+        float freq = 1000;
+        
         if(send) {
             // Send
+            try {
+                buf = new byte[ 1024 ];
+                af = new AudioFormat( (float )44100, 8, 1, true, false );
+                sdl = AudioSystem.getSourceDataLine( af );
+                sdl.open();
+                sdl.start();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            
+            ArrayList<Boolean> morse = new ArrayList<>();
+            
+            for(int i=0;i<msg.length();i++) {
+                char c = msg.charAt(i);
+                
+                if(c == '.') {
+                    for(int j=0;j<durationIndex(duration*1);j++) {
+                        morse.add(true);
+                    }
+                }
+                
+                if(c == '-') {
+                    for(int j=0;j<durationIndex(duration*3);j++) {
+                        morse.add(true);
+                    }
+                }
+                
+                if(c == ' ') {
+                    for(int j=0;j<durationIndex(duration*5);j++) {
+                        morse.add(false);
+                    }
+                }
+                
+                if(c == '/') {
+                    for(int j=0;j<durationIndex(duration*11);j++) {
+                        morse.add(false);
+                    }
+                }
+                
+                for(int j=0;j<durationIndex(duration*1);j++) {
+                    morse.add(false);
+                }
+            }
+            
+            for(int i=0;i<morse.size();i++) {
+                double angle = i / ( (float )44100 / freq ) * 2.0 * Math.PI;
+                buf[i%1024] = morse.get(i) ? (byte)( Math.sin( angle ) * 100 ) : 0;
+                if(i%1024 == 1023)
+                    sdl.write( buf, 0, 1024 );
+            }
+            sdl.write( buf, 0, morse.size()%1024+1 );
+            
+            sdl.drain();
+            sdl.stop();
+            
+            
+            frame.sendButton.setEnabled(true);
+            frame.receiveButton.setEnabled(true);
+
+            frame.pb.setIndeterminate(false);
         } else {
             // Receive
+            
         }
+        
+        done = true;
+    }
+    
+    public static int durationIndex(float duration) {
+        return Math.round(duration * (float )44100 / 1000);
+    }
+    
+    public static void wait(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ex) {}
+    }
+    
+    public static void wait(float millis) {
+        try {
+            Thread.sleep(Math.round(millis));
+        } catch (InterruptedException ex) {}
     }
 }
